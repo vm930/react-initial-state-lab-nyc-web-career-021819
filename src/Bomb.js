8d7545e24ef26ea028767828ce778204645ed569
Victoria Mei <vm930@hotmail.com>
2019-04-22 22:25:26 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			secondsLeft: this.props.initialCount
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.secondsLeft == 0 ? (
+					<div>Boom!</div>
+				) : (
+					<div>{this.state.secondsLeft} seconds left before I go boom!</div>
+				)}
+			</div>
+		);
+	}
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentSlideIndex: 0
+		};
+	}
+
+	render() {
+		return <div>I am on slide {this.state.currentSlideIndex}</div>;
+	}
+}
+
+export default ImageSlider;
